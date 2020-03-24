980c4ada351aff9a9d77503706ed9321bbb85e40
SAMSUNG <SAMSUNG@DESKTOP-71LONQQ>
2020-03-25 00:04:36 +0900

    get set 설정

--- src/main/java/com/example/demo/vo/CartVo.java
+++ src/main/java/com/example/demo/vo/CartVo.java
@@ -4,12 +4,35 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class CartVo {
 	private int no;
 	private String id;
 	private int gno;
 	private int qty;
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getGno() {
+		return gno;
+	}
+	public void setGno(int gno) {
+		this.gno = gno;
+	}
+	public int getQty() {
+		return qty;
+	}
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	
 }
--- src/main/java/com/example/demo/vo/MemberVo.java
+++ src/main/java/com/example/demo/vo/MemberVo.java
@@ -4,13 +4,42 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class MemberVo {
 	private String id;
 	private String name;
 	private String tel;
 	private int age;
-	private String pwd;	
+	private String pwd;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}	
+	
+	
 }
